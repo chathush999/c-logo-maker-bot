fa56e7979a423eb8043562d0e162cc5d8062e7cf
Chathush Nethmal Priyankara <102727399+chathush999@users.noreply.github.com>
2022-05-20 22:54:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 
-"/app/Github_Repo/null.py", line 1, in <module>
-2022-05-20T11:55:22.603821+00:00 app[worker.1]:     from null import app
-2022-05-20T11:55:22.603848+00:00 app[worker.1]: ImportError: cannot import name 'app' from partially initialized module 'null' (most likely due to a circular import) (/app/Github_Repo/null.py)
-
 from os import getenv
 from dotenv import load_dotenv
 
