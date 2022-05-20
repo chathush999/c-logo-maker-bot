5b91b7d8a3644eea73cd33612d93b993fc1e7504
Chathush Nethmal Priyankara <102727399+chathush999@users.noreply.github.com>
2022-05-20 17:33:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-[In reply to ▄︻╦Chathush Nethmal≡══]
+
 "/app/Github_Repo/null.py", line 1, in <module>
 2022-05-20T11:55:22.603821+00:00 app[worker.1]:     from null import app
 2022-05-20T11:55:22.603848+00:00 app[worker.1]: ImportError: cannot import name 'app' from partially initialized module 'null' (most likely due to a circular import) (/app/Github_Repo/null.py)
