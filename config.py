55de2e78553147338fb614a378bf358825b43d37
Chathush Nethmal Priyankara <102727399+chathush999@users.noreply.github.com>
2022-05-20 17:33:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,4 @@
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Get it from my.telegram.org
-API_ID = int(getenv("API_ID", "12995565"))
-API_HASH = getenv("API_HASH", "f43e63c6eb71cd9ee988e3b13a509f1a")
-
-## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "5247120991:AAE5-Tcb4mqAcVAdEsccc_C7T12K8sG06mw")
-
-# API By TechZBots || https://t.me/Mizutsuki_update
-LOGO_API_URL1 = "https://techzbotsapi.herokuapp.com/logo?text="
-
-LOGO_API_URL2 = "https://techzbotsapi.herokuapp.com/logo?square=true&text="
+[In reply to ▄︻╦Chathush Nethmal≡══]
+"/app/Github_Repo/null.py", line 1, in <module>
+2022-05-20T11:55:22.603821+00:00 app[worker.1]:     from null import app
+2022-05-20T11:55:22.603848+00:00 app[worker.1]: ImportError: cannot import name 'app' from partially initialized module 'null' (most likely due to a circular import) (/app/Github_Repo/null.py)
